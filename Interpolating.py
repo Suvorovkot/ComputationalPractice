3903fbac0f8a84f04825e16a216b53ece84abbbe
Paul <suvorovkot@gmail.com>
2018-04-11 20:06:39 +0300

    Smth

--- Interpolating.py
+++ Interpolating.py
@@ -1,14 +1,36 @@
 import math
+from prettytable import PrettyTable
 
-print('\n',"Algebraic interpolating. Interpolating Lagrange and Newton polynomial",'\n')
+print('\n', "Algebraic interpolating. Interpolating Lagrange and Newton polynomial", '\n')
 
 def f(x):
     return math.exp(-x)-x**2/2
 
-m = 15; n = 7; a = 0; b = 1; x = 0.6
+print('\n', "Test task: m = 15, x = 0.6, n = 7", '\n')
+
+print('\n', "Please, enter number of nods", '\n')
+m = int(input())
+a = 0; b = 1
 
 #Nods initialising
 nods = []
-for i in range(0,m+1):
+for i in range(0, m+1):
     nods.append(a + (b - a) * i / m)
-#print(nods)
+
+#Creating table
+Init = PrettyTable()
+Init.field_names = ["i", "Xi", "f(Xi)"]
+for i in range(0, m+1):
+    Init.add_row([i, nods[i], f(nods[i])])
+print(Init, 'n')
+
+print('\n', "Please, enter x value", '\n')
+x = float(input())
+print('\n', "Please, enter degree of interpolating polynomial", '\n')
+n = int(input())
+
+#Interpolation
+def sortByResidual(n):
+    return x - n
+nods.sort(key = sortByResidual)
+print(nods, '\n')
