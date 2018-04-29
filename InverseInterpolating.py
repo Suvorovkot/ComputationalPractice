f3dad0bc3b6f760c786e3291839afb63c6230740
Paul <suvorovkot@gmail.com>
2018-04-29 17:23:39 +0300

    Derrivatives table added

--- InverseInterpolating.py
+++ InverseInterpolating.py
@@ -10,7 +10,11 @@ warnings.simplefilter('ignore', np.RankWarning)
 print('\n', "Inverse interpolation. Finding the derivatives of a table-defined function", '\n')
 
 def f(x):
-    return np.exp(-x)-x**2/2
+    return np.exp(-x) - x**2/2
+def derf(x):
+    return -np.exp(-x) - x
+def secDerf(x):
+    return np.exp(-x) - 1
 
 def approxDer1(y, h):
     return (f(y + h) - f(y - h)) / (2 * h)
@@ -59,8 +63,8 @@ b = float(seg[1])
 
 #Nods initialising
 nods = []
-for i in range(1, m + 2):
-    nods.append(a + ((b - a) / m) * (i - 1))
+for i in range(0, m + 1):
+    nods.append(a + ((b - a) / m) * i)
 
 #Creating initial table
 Init = PrettyTable()
@@ -95,29 +99,31 @@ coefNewton = []
 for i in range(1, deg+1):
     coefNewton.append(dividedResidue(fnods[:i]))
 Q = polNewton(coefNewton, F, fnods[:deg])
-print("Newtons's: Qn(F) = ", Q)
-print("|F - Qn(F)| = ", np.fabs(F-Q))
+print("Newtons's: X = Qn(F) = ", Q)
+print("|F - f(X)| = ", np.fabs(F-f(Q)))
 print("--------------", '\n')
 
 print("--------------")
 print("Second way of inverse interpolation")
 #Second way (we don't know much about monotone)
+fnods = []
+for nd in nods:
+    fnods.append(f(nd))
 print("Please, enter accuracy for finding roots", '\n')
 e = float(input())
+
 #RootSep
-eps = 6
 n = 50.0
 h = (b-a)/n
 x1 = a; x2 = x1 + h
 k = 0
 ints = []
 while(x2 <= b):
-    y1 = polLagrange(deg, x1, fnods, nods)
-    y2 = polLagrange(deg, x2, fnods, nods)
+    y1 = polLagrange(deg, x1, fnods, nods) - F
+    y2 = polLagrange(deg, x2, fnods, nods) - F
     if (y1 * y2 <= 0):
         k += 1
-        #ints.append((x1,x2))
-        ints.append((round(x1, eps),round(x2, eps)))
+        ints.append((x1,x2))
     x1 = x2
     x2 += h
 #print('\n',"Number of intervals:", k)
@@ -131,16 +137,16 @@ for i in range(0, k):
     a = ints[i][0]; b = ints[i][1]
     x = (a + b) / 2
     #print("For x", i, ' initial approximation is: ', x, sep="")
-    count = 1
-    while np.fabs(polLagrange(deg, x, fnods, nods)) >= e:
-        a, b = (a, x) if polLagrange(deg, a, fnods, nods) * polLagrange(deg, x, fnods, nods) < 0 else (x, b)
+    #count = 1
+    while np.fabs(polLagrange(deg, x, fnods, nods) - F) >= e:
+        a, b = (a, x) if (polLagrange(deg, a, fnods, nods) - F) * (polLagrange(deg, x, fnods, nods) - F) < 0 else (x, b)
         x = (a + b) / 2
-        count += 1
+        #count += 1
     #print("x",i,' = ',x, sep="")
-    print("Lagrange's: Qn(F) = ", Q)
-    print("|F - Qn(F)| = ", np.fabs(F - Q))
+    print("Lagrange's: X = Qn(F) = ", x)
+    print("|F - f(X)| = ", np.fabs(F - f(x)))
     #print('\n',"With",count,"iterations.",'\n')
-    print("--------------", '\n')
+print("--------------", '\n')
 
 print("--------------")
 print("Finding derrivatives")
@@ -148,7 +154,9 @@ h = (b - a) / m
 
 #Creating derrrivative table
 Init = PrettyTable()
-Init.field_names = ["i", "Xi", "f(Xi)"]
-for i in range(0, m+1):
-    Init.add_row([i, nods[i], f(nods[i])])
+Init.field_names = ["i", "Xi", "f(Xi)", "f'_a(Xi)", "|f'(Xi)-f'_a(Xi)|", "f''_a(Xi)", "|f''(Xi)-f''_a(Xi)|"]
+Init.add_row([0, nods[0], f(nods[0]), approxDer2(nods[0], h), abs(derf(nods[0])-approxDer2(nods[0], h)), approxDer4(nods[0], h), abs(secDerf(nods[0])-approxDer4(nods[0], h))])
+for i in range(1, m):
+    Init.add_row([i, nods[i], f(nods[i]), approxDer1(nods[i], h), abs(derf(nods[0])-approxDer2(nods[i], h)), approxDer4(nods[i], h), abs(secDerf(nods[i])-approxDer4(nods[i], h))])
+Init.add_row([m, nods[m], f(nods[m]), approxDer3(nods[m], h), abs(derf(nods[m])-approxDer3(nods[m], h)), approxDer4(nods[m], h), abs(secDerf(nods[m])-approxDer4(nods[m], h))])
 print(Init, '\n')
