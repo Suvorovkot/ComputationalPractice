a26b8871e9e5540277091ef7d06db2f662ca48d8
Paul <suvorovkot@gmail.com>
2018-09-13 11:31:26 +0300

    Gaussian mrthod addded

--- DifferentialEquation.py
+++ DifferentialEquation.py
@@ -10,7 +10,7 @@ print('\n', "Finding solution for Cauchy problem for first-order ODE", '\n')
 def f(y):
     return (-3) * y + y ** 2
 
-h = 0.1
+h = 0.01
 N = 10
 x0 = 0
 y0 = [1., -2., 2., 6., -30., -42.]
@@ -19,11 +19,10 @@ def solution(x):
     return 3 / (np.exp(3*x + np.log(2)) + 1)
 
 def Taylor(x, x0, n):
-    p = 1
+    p = y0[0]
     for i in range(1, n+1):
         p += ((y0[i]) / (fac(i))) * (x-x0)**i
     return p
-
 
 
 #Creating table for accurate solution
@@ -45,44 +44,69 @@ for i in range(0, 5):
     fprimes.append(Taylor(nods[i], x0, 5))
     Init.add_row([(i-2), nods[i], fprimes[i]])
 print(Init, '\n')
+for j in range(5):
+    print("|y", j, "- y(x",j ,")| = ", abs(fprimes[j]-solution(nods[j])))
 
 #Adding Adams
 print('\n', "Adams method", '\n')
-for i in range(5, N+1):
+Init = PrettyTable()
+Init.field_names = ["i", "Xi", "y(Xi)"]
+for i in range(5, N+3):
     fprimes.append(fprimes[i-1] + 1/720 * h * (1901*f(fprimes[i-1]) - 2774*f(fprimes[i-2]) + 2616*f(fprimes[i-3]) - 1274*f(fprimes[i-4]) + 251*f(fprimes[i-5])))
     Init.add_row([i-2, nods[i], fprimes[i]])
 print(Init, '\n')
+print("|y10 - y(x10)| = ", abs(fprimes[10]-solution(nods[10])))
+
+#Reinitialise nods
+nods = []
+for k in range(N+1):
+    nods.append(x0 + k * h)
 
 #Euler method
 print('\n', "Euler's method", '\n')
 Init = PrettyTable()
 fprimes = [y0[0]]
 Init.field_names = ["i", "Xi", "y(Xi)"]
-for i in range(0, N):
-    fprimes.append(fprimes[i] + h * f(fprimes[i]))
+for i in range(1, N+1):
+    fprimes.append(fprimes[i-1] + h * f(fprimes[i-1]))
     Init.add_row([i, nods[i], fprimes[i]])
 print(Init, '\n')
+print("|y10 - y(x10)| = ", abs(fprimes[9]-solution(nods[9])))
 
 #Improvement
 print('\n', "Improved Euler's method", '\n')
 Init = PrettyTable()
 fprimes = [y0[0]]
 Init.field_names = ["i", "Xi", "y(Xi)"]
-for i in range(0, N):
-    fprimes.append(fprimes[i] + (h/2) * (f(fprimes[i]) + f(fprimes[i] + h*f(fprimes[i]))))
+for i in range(1, N+1):
+    fprimes.append(fprimes[i-1] + (h/2) * (f(fprimes[i-1]) + f(fprimes[i-1] + h*f(fprimes[i-1]))))
     Init.add_row([i, nods[i], fprimes[i]])
 print(Init, '\n')
+print("|y10 - y(x10)| = ", abs(fprimes[9]-solution(nods[9])))
+
+#Improvement2
+print('\n', "Improved Euler's method 2", '\n')
+Init = PrettyTable()
+fprimes = [y0[0]]
+Init.field_names = ["i", "Xi", "y(Xi)"]
+for i in range(1, N+1):
+    fprimes.append(fprimes[i-1] + h * f(fprimes[i-1] + (h/2)*f(fprimes[i-1])))
+    Init.add_row([i, nods[i], fprimes[i]])
+print(Init, '\n')
+print("|y10 - y(x10)| = ", abs(fprimes[9]-solution(nods[9])))
+
 
 #R-K method
 print('\n', "Runge-Kutt method", '\n')
 Init = PrettyTable()
 fprimes = [y0[0]]
 Init.field_names = ["i", "Xi", "y(Xi)"]
-for i in range(0, N):
-    k1 = h*f(fprimes[i])
-    k2 = h*f(fprimes[i]+(k1/2))
-    k3 = h*f(fprimes[i]+(k2/2))
-    k4 = h*f(fprimes[i]+(k3))
-    fprimes.append(fprimes[i] + (1/6) * (k1 + 2*k2 + 2*k3 + k4))
+for i in range(1, N+1):
+    k1 = h*f(fprimes[i-1])
+    k2 = h*f(fprimes[i-1] + (k1/2))
+    k3 = h*f(fprimes[i-1] + (k2/2))
+    k4 = h*f(fprimes[i-1] + k3)
+    fprimes.append(fprimes[i-1] + (1/6) * (k1 + 2*k2 + 2*k3 + k4))
     Init.add_row([i, nods[i], fprimes[i]])
 print(Init, '\n')
+print("|y10 - y(x10)| = ", abs(fprimes[9]-solution(nods[9])))
---
+++ GaussianMethod.py
@@ -0,0 +1,175 @@
+import numpy
+import copy
+
+eps = 1e-16
+
+def gaussFunc(a):
+
+    c = numpy.array(a)
+    a = numpy.array(a)
+
+    len1 = len(a[:, 0])
+    len2 = len(a[0, :])
+    vectB = copy.deepcopy(a[:, len1])
+
+    for i in range(len1):
+        #leadEl(a, i, len1)   #Chooses leading element in columns and swap lines
+
+        alead = float(a[i][i])
+
+        z = i
+        while z < len2:
+            a[i][z] = a[i][z] / alead
+            z += 1
+
+        j = i + 1
+
+        while j < len1:
+            b = a[j][i]
+            z = i
+
+            while z < len2:
+                a[j][z] = a[j][z] - a[i][z] * b
+                z += 1
+            j += 1
+
+    a = backTrace(a)
+
+    print("b - Ax:")
+    print(vectorN(c, a, len1, vectB))
+
+    return a
+
+def leadEl(a, i, len):
+    max = abs(a[i][i])
+    mi = i
+    t1 = i
+    while t1 < len:
+        if abs(a[t1][i]) > max:
+            max = abs(a[t1][i])
+            mi = t1
+        t1 += 1
+    if abs(max) < eps:
+        raise DetermExeption("Check matrix")  # det(A) = 0
+    swapLines(a, i, mi)
+
+def swapLines (a, i, j):
+    if i != j:
+        a[j], a[i] = copy.deepcopy(a[i]), copy.deepcopy(a[j])
+
+class DetermExeption(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+def backTrace(a):
+    len1 = len(a[:, 0])
+    len2 = len(a[0, :])
+
+    a = numpy.array(a)
+    i = len1 - 1
+    while i > 0:
+        j = i - 1
+        while j >= 0:
+            a[j][len1] = a[j][len1] - a[j][i] * a[i][len1]
+            j -= 1
+        i -= 1
+
+    return a[:, len2 - 1]
+
+
+def vectorN(c, a, rank, vectB):  # c = A, a - answer
+    c = numpy.array(c)
+    a = numpy.array(a)
+    vectB = numpy.array(vectB)
+
+    b = numpy.zeros((rank))
+
+    i = 0
+
+
+    while i < rank:
+        j = 0
+        while j < rank:
+
+            b[i] += c[i][j]*a[j]
+            j += 1
+        i = i+1
+
+    c = copy.deepcopy(b)
+
+    for i in range(rank):
+        c[i] = abs(c[i] - vectB[i])
+
+    return c
+
+def invMat(a):
+    a = numpy.array(a)
+
+    len1 = len(a[:, 0])
+
+    E = numpy.array([[1, 0, 0],
+                     [0, 1, 0],
+                     [0, 0, 1]])
+    a = numpy.hstack((a, E))
+    len2 = len(a[0, :])
+    for i in range(len1):
+        leadEl(a, i, len1)   #Chooses leading element in columns and swaps lines
+
+        alead = float(a[i][i])
+
+        z = i
+        while z < len2:
+            a[i][z] = a[i][z] / alead
+            z += 1
+
+        j = i + 1
+
+        while j < len1:
+            b = a[j][i]
+            z = i
+            while z < len2:
+                a[j][z] = a[j][z] - a[i][z] * b
+                z += 1
+            j += 1
+    for i in range(1, len1):
+        j = i - 1
+        b = a[j][i]
+        while j >= 0:
+            z = i
+            while z < len2:
+                a[j][z] -= a[i][z] * b
+                z += 1
+            j -= 1
+    return a
+
+#a = numpy.array([[1., 2, 1, 1],
+#                [1, 3 , 1, 1],
+#                [0, 1, 1, 2]])
+#a = numpy.array([[1.239 ,  1.6239, -0.4239,  0.4239],
+#                [2.239,   1.239,  -1.5239,  1.5239],
+#                [0.239,   0.566,   1.566,   1.566]])
+a = numpy.array([[8.29381, 0.995516, -0.560617, 1],
+                [0.995516, 6.298198, 0.595772, 0],
+                [-0.560617, 0.595772, 4.997407, 0]])
+# a = numpy.array([[0.0000829381, 0.995516, -0.560617, 1],
+#                 [0.995516, 6.298198, 0.595772, 0],
+#                 [-0.560617, 0.595772, 4.997407, 0]])
+print("A|b:")
+print(a)
+
+
+d = invMat(a)
+print("\n")
+print("A^(-1):")
+for t in range(len(d[:, 0])):
+    print(d[t][len(d[0, :])-len(d[:, 0]):len(d[0, :])])
+print("\n")
+
+b = gaussFunc(a)
+print("\n")
+print("Answer:")
+print(b)
